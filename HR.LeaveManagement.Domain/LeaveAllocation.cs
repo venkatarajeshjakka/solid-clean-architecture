c32970dd5bd4f5a7421934fbb97ce50eb32b3469
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-06-20 22:49:18 +0530

    initial commit

---
+++ HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿namespace HR.LeaveManagement.Domain;
+
+public class LeaveAllocation
+{
+    public int Id { get; set; }
+
+    public int NumberOfDays { get; set; }
+
+    public LeaveType? LeaveType { get; set; }
+
+    public int LeaveTypeId { get; set; }
+
+    public int Period { get; set; }
+}
+
+
