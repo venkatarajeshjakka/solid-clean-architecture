16c83cf3116e83662020915f388f19ef4080550c
Venkata Rajesh Jakka <50277820+venkatarajeshjakka@users.noreply.github.com>
2023-06-30 11:36:28 +0530

    Added Infra Project (#3)
    * Added Infra Project
    
    * Added package for Service Collection
    
    * Added email and logger

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandValidator.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistance;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
+{
+    public class UpdateLeaveTypeCommandValidator : AbstractValidator<UpdateLeaveTypeCommand>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public UpdateLeaveTypeCommandValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+
+            RuleFor(p => p.Id)
+                .NotNull()
+                .MustAsync(LeaveTypeMustExist);
+
+            RuleFor(p => p.Name).NotEmpty()
+            .WithMessage("{PropertyName} is required")
+            .NotNull()
+            .MaximumLength(70)
+            .WithMessage("{PropertyName} must be fewer than 70 characters");
+
+            RuleFor(p => p.DefaultDays)
+            .LessThan(100).WithMessage("{PropertyName} cannot exceed 100")
+            .GreaterThan(1).WithMessage("{PropertyName} cannot be less than 1");
+
+            RuleFor(q => q).MustAsync(LeaveTypeUnique)
+            .WithMessage("Leave type already exists");
+        }
+
+
+
+        private async Task<bool> LeaveTypeMustExist(int id, CancellationToken token)
+        {
+            var leaveType = await _leaveTypeRepository.GetByIdAsync(id);
+            return leaveType != null;
+        }
+
+        private Task<bool> LeaveTypeUnique(UpdateLeaveTypeCommand command, CancellationToken token)
+        {
+            return _leaveTypeRepository.IsLeaveTypeUnique(command.Name);
+        }
+    }
+}
+
---
+++ src/Infrastructure/Hr.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Hr.LeaveManagement.Infrastructure.EmailService;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hr.LeaveManagement.Infrastructure;
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+        services.AddTransient<IEmailSender, EmailSender>();
+
+        return services;
+    }
+
+}
+
