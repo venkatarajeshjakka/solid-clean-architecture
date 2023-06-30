522c1c968f39c0a0f871673551b54a79e951f166
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-06-30 11:34:55 +0530

    Added email and logger

--- src/Infrastructure/Hr.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ src/Infrastructure/Hr.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Hr.LeaveManagement.Infrastructure.EmailService;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Hr.LeaveManagement.Infrastructure;
@@ -6,6 +9,10 @@ public static class InfrastructureServicesRegistration
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+        services.AddTransient<IEmailSender, EmailSender>();
+
         return services;
     }
 
