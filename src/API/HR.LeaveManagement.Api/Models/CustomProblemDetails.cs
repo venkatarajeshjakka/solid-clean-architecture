6bf01a7e2a52f97a4e15cfdfa40951d307804f41
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-07-09 23:59:55 +0530

    Added Basic Code for Exception Handler

---
+++ src/API/HR.LeaveManagement.Api/Models/CustomProblemDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.LeaveManagement.Api.Models
+{
+    public class CustomProblemDetails : ProblemDetails
+    {
+        public IDictionary<string, string[]> Errors { get; set; } = new Dictionary<string, string[]>();
+    }
+}
+
