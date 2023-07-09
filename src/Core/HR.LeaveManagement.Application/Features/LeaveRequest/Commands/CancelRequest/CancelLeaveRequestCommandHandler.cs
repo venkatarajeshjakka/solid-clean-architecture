2fd68a227667178dfce4284f1283ac2a42deabaf
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-07-09 23:50:19 +0530

    Added Leave Request Code

---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CancelRequest/CancelLeaveRequestCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistance;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Models.Email;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CancelRequest
+{
+    public class CancelLeaveRequestCommandHandler : IRequestHandler<CancelLeaveRequestCommand, Unit>
+    {
+        private readonly IEmailSender _emailSender;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IAppLogger<UpdateLeaveRequest.CancelLeaveRequestCommandHandler> _appLogger;
+
+
+        public CancelLeaveRequestCommandHandler(IEmailSender emailSender, ILeaveRequestRepository leaveRequestRepository, IAppLogger<UpdateLeaveRequest.CancelLeaveRequestCommandHandler> appLogger)
+        {
+            _emailSender = emailSender;
+            _leaveRequestRepository = leaveRequestRepository;
+            _appLogger = appLogger;
+        }
+
+        public async Task<Unit> Handle(CancelLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+            if (leaveRequest is null)
+                throw new NotFoundException(nameof(leaveRequest), request.Id);
+
+            leaveRequest.Cancelled = true;
+
+            try
+            {
+                var email = new EmailMessage
+                {
+                    To = string.Empty,
+                    Body = $"Your leave request for {leaveRequest.StartDate:D} to {leaveRequest.EndDate:D} " + $"has been cancelled successfully.",
+                    Subject = "Leave Request Submitted"
+                };
+
+                await _emailSender.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                _appLogger.LogWarning(ex.Message);
+
+            }
+
+            return Unit.Value;
+        }
+    }
+}
+
---
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaverequestCommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistance;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.UpdateLeaveRequest;
+using HR.LeaveManagement.Application.Models.Email;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest
+{
+    public class CreateLeaverequestCommandHandler : IRequestHandler<CreateLeaveRequestCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly IEmailSender _emailSender;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IAppLogger<CreateLeaverequestCommandHandler> _appLogger;
+
+        public CreateLeaverequestCommandHandler(IMapper mapper, IEmailSender emailSender,
+            ILeaveRequestRepository leaveRequestRepository, ILeaveTypeRepository leaveTypeRepository, IAppLogger<CreateLeaverequestCommandHandler> appLogger)
+        {
+            _mapper = mapper;
+            _emailSender = emailSender;
+            _leaveRequestRepository = leaveRequestRepository;
+            _leaveTypeRepository = leaveTypeRepository;
+            _appLogger = appLogger;
+        }
+
+        public async Task<Unit> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateLeaveRequestCommandValidator(_leaveTypeRepository);
+
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Any())
+                throw new BadRequestException("Invalid Leave Request", validationResult);
+
+            var leaveRequest = _mapper.Map<Domain.LeaveRequest>(request);
+
+            await _leaveRequestRepository.CreateAsync(leaveRequest);
+            try
+            {
+                var email = new EmailMessage
+                {
+                    To = string.Empty,
+                    Body = $"Your leave request for {request.StartDate:D} to {request.EndDate:D} " + $"has been updated successfully.",
+                    Subject = "Leave Request Submitted"
+                };
+
+                await _emailSender.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                _appLogger.LogWarning(ex.Message);
+
+            }
+
+            return Unit.Value;
+        }
+    }
+}
+
