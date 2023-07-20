14c74b3ee237e08059eddf228a51878921c753db
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-07-20 21:49:03 +0530

    Added Serilog feature

--- src/Core/HR.LeaveManagement.Domain/Common/BaseEntity.cs
+++ src/Core/HR.LeaveManagement.Domain/Common/BaseEntity.cs
@@ -7,5 +7,9 @@ public abstract class BaseEntity
     public DateTime? DateCreated { get; set; }
 
     public DateTime? DateModified { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public string? ModifiedBy { get; set; }
 }
 
