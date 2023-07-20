076d354b5568c77ecb4986b57dd49879aeb30f8f
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-07-20 14:08:37 +0530

    Added Authorize Attribute

--- src/API/HR.LeaveManagement.Api/Controllers/LeaveRequestsController.cs
+++ src/API/HR.LeaveManagement.Api/Controllers/LeaveRequestsController.cs
@@ -6,6 +6,7 @@ using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.UpdateLeaveR
 using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetails;
 using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,6 +15,7 @@ namespace HR.LeaveManagement.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class LeaveRequestsController : ControllerBase
     {
         private readonly IMediator _mediator;
